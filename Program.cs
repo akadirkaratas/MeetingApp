bc6a69739e82bab250efcd92d671e0600d6a6585
Kadir Karataş <a.kadir.karatass@gmail.com>
2025-09-30 00:21:52 +0300

    LibMan yapılandırması eklendi, layout dosyaları güncellendi

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+app.UseStaticFiles();
 
+app.UseRouting();
 //app.MapDefaultControllerRoute();
 
 app.MapControllerRoute(
