21de2339aaf1de7eea1850edaf14b794dfb64b3b
Kadir Karataş <a.kadir.karatass@gmail.com>
2025-10-01 23:40:44 +0300

    form validations tamamlandi, projenin finali

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MeetingApp.Models
 {
     public class UserInfo
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Ad alani zorunlu!")]
         public string? Name { get; set; }
-       
+
+        [Required(ErrorMessage ="Telefon alani zorunlu!")]
+        
         public string? Phone { get; set; }
 
+        [Required(ErrorMessage ="Email alani zorunlu!")]
+        [EmailAddress(ErrorMessage ="Gecersiz email adresi!")]
         public string? Email { get; set; }
 
+        [Required(ErrorMessage ="Secim yapmalisiniz!")]
         public bool? WillAttend { get; set; }
     }
 }
